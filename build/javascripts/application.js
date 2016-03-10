3651de1794df6a0d9f2a9bb76b3da5e431a6cf8f
Rafael Mariano <toruticas@gmail.com>
2016-03-10 00:09:10 -0300

    Version 0.1 created.

--- build/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var Run;
-  angular.module('ritasoares', ['ngMessages', 'ngAnimate', 'ui.bootstrap', 'ui.router', 'uiGmapgoogle-maps']);
-  Run = function($rootScope, $location) {
-    return $rootScope.localTime = new Date();
-  };
-  Run.$inject = ['$rootScope', '$location'];
-  return angular.module('ritasoares').run(Run);
-})();
